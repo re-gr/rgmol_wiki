566740f0290efde6dc40045ec54becda4ad8d45d
Rg <remi.grincourt@ens-lyon.fr>
2025-03-31 11:22:26 +0200

    Corrected small bug for fukui functions

--- doc/tuto/cdft-1.py
+++ doc/tuto/cdft-1.py
@@ -6,6 +6,6 @@ mol = rgmol.extract_molden.extract(file,do_find_bonds=1)
 rgmol.extract_orca.extract_transition(rgmol.examples.orca_H2CO,mol=mol)
 
 file_m = rgmol.examples.molden_H2CO_m
-mol_m = rgmol.extract_molden.extract(file,do_find_bonds=1)
+mol_m = rgmol.extract_molden.extract(file_m,do_find_bonds=1)
 
 mol.plot_fukui_function(mol_m=mol_m,fukui_type="f-",grid_points=(100,100,100),delta=10)
