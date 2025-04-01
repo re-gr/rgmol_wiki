61b9ca0bad3871a475a07795a1a189cfc002decb
Rg <remi.grincourt@ens-lyon.fr>
2025-04-01 11:27:41 +0200

    Updated wiki for 0.1.2.4

--- doc/quick_start-2.py
+++ doc/quick_start-2.py
@@ -1,3 +1,15 @@
-import pyvista
-sphere = pyvista.Sphere()
-out = sphere.plot()
+import rgmol
+import rgmol.examples
+
+list_molecules = []
+
+file = rgmol.examples.adf_CH4
+mol = rgmol.extract_adf.extract(file)
+list_molecules.append(mol)
+
+file2 = rgmol.examples.adf_CH3Cl
+mol2 = rgmol.extract_adf.extract(file)
+list_molecules.append(mol2)
+
+group_mol = rgmol.group_molecules(list_molecules)
+group_mol.plot_radius()
